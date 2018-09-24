9765fb9e565f87884ac60a43276e789551d5c694
Steven Leiva <leiva.steven@gmail.com>
2018-09-23 19:04:49 -0500

    Parse logs in french format.
    This is an exercise in doing *almost* exactly what we did in order to
    parse logs that are in the US format.
    
    The one cool thing that we did is that we derived an instace of `Enum`
    for our `Product` data type, which makes it easy to write a function
    with the type `Interger -> Product`.

--- src/ParsingLogFiles.hs
+++ src/ParsingLogFiles.hs
@@ -3,6 +3,7 @@
 module ParsingLogFiles where
 
 import Data.Word (Word8)
+import Data.Char (digitToInt)
 import Data.Time (LocalTime(..), fromGregorian, TimeOfDay(..))
 import Data.Attoparsec.ByteString.Char8 (char, digit, string, endOfLine, Parser, parseOnly, many', count, decimal, option)
 import Control.Applicative ((<|>))
@@ -10,7 +11,10 @@ import qualified Data.ByteString as B
 
 data IP = IP Word8 Word8 Word8 Word8 deriving Show
 
-data Product = Mouse | Keyboard | Monitor | Speakers deriving Show
+data Product = Mouse | Keyboard | Monitor | Speakers deriving (Show, Enum)
+
+productFromID :: Int -> Product
+productFromID id = toEnum (id - 1)
 
 data Source = Internet | Friend | NoAnswer deriving Show
 
@@ -23,19 +27,30 @@ data LogEntry = LogEntry { entryTime :: LocalTime
 type Log = [LogEntry]
 
 main :: IO ()
-main = print $ parseOnly logParser (logs <> "\n" <> logsWithSource)
+main = print $ parseOnly logParser logsInFrench
 
 logParser :: Parser Log
-logParser = many' $ logEntryParser <* endOfLine
+logParser = many' $ logEntryParserFrench <* endOfLine
 
 logEntryParser :: Parser LogEntry
 logEntryParser = do
-  entryTime <- timeParser
+  entryTime    <- timeParser
   char ' '
-  entryIP <- ipParser
+  entryIP      <- ipParser
   char ' '
   entryProduct <- productParser
-  entrySource <- option NoAnswer $ char ' ' >> sourceParser
+  entrySource  <- option NoAnswer $ char ' ' >> sourceParser
+  return $ LogEntry entryTime entryIP entryProduct entrySource
+
+logEntryParserFrench :: Parser LogEntry
+logEntryParserFrench = do
+  entryIP <- ipParser
+  char ' '
+  entryTime <- timeParserEuropean
+  char ' '
+  entryProduct <- (productFromID . digitToInt) <$> digit
+  char ' '
+  entrySource <- sourceParser
   return $ LogEntry entryTime entryIP entryProduct entrySource
 
 timeParser :: Parser LocalTime
@@ -54,6 +69,24 @@ timeParser = do
   return $ LocalTime { localDay = fromGregorian (read y) (read mm) (read d)
                      , localTimeOfDay = TimeOfDay (read h) (read m) (read s)
                      }
+
+timeParserEuropean :: Parser LocalTime
+timeParserEuropean = do
+  d   <- count 2 digit
+  char '/'
+  m   <- count 2 digit
+  char '/'
+  y   <- count 4 digit
+  char ' '
+  h   <- count 2 digit
+  char ':'
+  mm  <- count 2 digit
+  char ':'
+  s   <- count 2 digit
+  return $ LocalTime { localDay = fromGregorian (read y) (read mm) (read d)
+                     , localTimeOfDay = TimeOfDay (read h) (read m) (read s)
+                     }
+
 
 ipParser :: Parser IP
 ipParser = do
@@ -110,3 +143,11 @@ logsWithSource = B.concat [ "2013-06-29 11:16:23 124.67.34.60 keyboard internet\
                           , "2013-06-29 12:51:50 101.40.50.62 keyboard internet\n"
                           , "2013-06-29 13:10:45 103.29.60.13 mouse internet"
                           ]
+
+logsInFrench :: B.ByteString
+logsInFrench = B.concat [ "154.41.32.99 29/06/2013 15:32:23 4 internet\n"
+                         , "76.125.44.33 29/06/2013 16:56:45 3 noanswer\n"
+                         , "123.45.67.89 29/06/2013 18:44:29 4 friend\n"
+                         , "100.23.32.41 29/06/2013 19:01:09 1 internet\n"
+                         , "151.123.45.67 29/06/2013 20:30:13 2 internet"
+                         ]
