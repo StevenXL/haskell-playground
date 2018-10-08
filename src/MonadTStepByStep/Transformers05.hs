d943b87e479aa9f8bb21979f0dcf0961c6603cbd
Steven Leiva <leiva.steven@gmail.com>
2018-10-08 07:57:43 -0500

    Add loggin of variable names.
    I had a lot of trouble figuring out where in the stack the WriterT monad
    transformer belonged. I doubt I could have figured this out on my own.

--- src/MonadTStepByStep/Transformers05.hs
+++ src/MonadTStepByStep/Transformers05.hs
@@ -1,10 +1,11 @@
 -- Monad Transformers - Step By Step (https://page.mi.fu-berlin.de/scravy/realworldhaskell/materialien/monad-transformers-step-by-step.pdf)
-module MonadTStepByStep.Transformers04 where
+module MonadTStepByStep.Transformers05 where
 
 import           Control.Monad.Except   (ExceptT, runExceptT, throwError)
 import           Control.Monad.Identity (Identity, runIdentity)
 import           Control.Monad.Reader   (ReaderT, ask, local, runReaderT)
-import           Control.Monad.State    (StateT, runStateT, MonadState(..))
+import           Control.Monad.State    (MonadState (..), StateT, runStateT)
+import           Control.Monad.Writer   (WriterT, runWriterT, tell)
 import           Data.Map               (Map)
 import qualified Data.Map               as M
 import           Data.Maybe             (fromJust, maybe)
@@ -29,36 +30,39 @@ exampleExp = Plus (Lit 1) (Abs "x" (Var "x"))
 
 type InterpreterError = String
 type AppState = Int
+type Log = [String]
 
--- Step 7: Add the state effect
-type Eval4 a = ReaderT Env (ExceptT InterpreterError (StateT AppState Identity)) a
+-- Step 8: Add loggin capabilities
+type Eval5 a = ReaderT Env (ExceptT InterpreterError (WriterT Log (StateT AppState Identity))) a
 
-runEval4 :: Env -> AppState -> Eval4 a -> (Either InterpreterError a, AppState)
-runEval4 env appState eval = runIdentity $ runStateT ( runExceptT $ runReaderT eval env ) appState
+runEval5 :: Env -> AppState -> Eval5 a -> ((Either InterpreterError a, Log), AppState)
+runEval5 env appState eval = runIdentity $ runStateT ( runWriterT $ runExceptT $ runReaderT eval env ) appState
 
-eval4 :: Exp -> Eval4 Value
-eval4 (Lit i) = tick >> (return $ IntVal i)
-eval4 (Var name) = do
+eval5 :: Exp -> Eval5 Value
+eval5 (Lit i) = tick >> (return $ IntVal i)
+eval5 (Var name) = do
   tick
+  tell [name]
   env <- ask
   maybe (throwError $ "Unbound variable: " ++ name) return (M.lookup name env)
-eval4 (Plus exp exp') = do
+eval5 (Plus exp exp') = do
   tick
-  v1 <- eval4 exp
-  v2 <- eval4 exp'
+  v1 <- eval5 exp
+  v2 <- eval5 exp'
   case (v1, v2) of
     (IntVal i, IntVal i') -> return (IntVal $ i + i')
     _ -> throwError $ "Type Error: " ++ show v1 ++ " or " ++ show v2 ++ " is not an IntVal"
-eval4 (Abs name exp) = do
+eval5 (Abs name exp) = do
   tick
+  tell [name]
   env <- ask
   return $ FunVal env name exp
-eval4 (App exp exp') = do
+eval5 (App exp exp') = do
   tick
-  fun <- eval4 exp
-  val2 <- eval4 exp'
+  fun <- eval5 exp
+  val2 <- eval5 exp'
   case fun of
-    FunVal env' n body -> local (const $ M.insert n val2 env' ) (eval4 body)
+    FunVal env' n body -> local (const $ M.insert n val2 env' ) (eval5 body)
     _ -> throwError $ "Type Error: " ++ show fun ++ " is not a function"
 
 tick :: (Num s, MonadState s m) => m ()
@@ -66,5 +70,5 @@ tick = do
   calls <- get
   put (calls + 1)
 
-test4 :: IO ()
-test4 = print $ runEval4 M.empty 0 (eval4 exampleExp)
+test5 :: IO ()
+test5 = print $ runEval5 M.empty 0 (eval5 exampleExp)
