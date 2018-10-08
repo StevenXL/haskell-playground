6227e1677bddf1deac514bfa0ad314656471616c
Steven Leiva <leiva.steven@gmail.com>
2018-10-07 19:01:56 -0500

    Add a (partial) interpreter function

--- src/MonadTStepByStep/Transformers.hs
+++ src/MonadTStepByStep/Transformers.hs
@@ -1,8 +1,9 @@
 -- Monad Transformers - Step By Step (https://page.mi.fu-berlin.de/scravy/realworldhaskell/materialien/monad-transformers-step-by-step.pdf)
 module MonadTStepByStep.Transformers where
 
+import Data.Maybe (fromJust)
 import           Data.Map (Map)
-import qualified Data.Map as Map
+import qualified Data.Map as M
 
 -- STEP 1: Define the types of our programming language.
 
@@ -18,3 +19,16 @@ data Exp = Lit Integer
          deriving (Show)
 
 data Value = IntVal Integer | FunVal Env Name Exp deriving (Show)
+
+-- STEP 2: Write an evaluation / interpreter function for our programming
+-- language. Naturally, the type has to be Env -> Exp -> Value
+eval0 :: Env -> Exp -> Value
+eval0 _ (Lit i) = IntVal i
+eval0 env (Var name) = fromJust (M.lookup name env)               -- partiality
+eval0 env (Plus exp exp') = let IntVal i = eval0 env exp          -- partiality
+                                IntVal i' = eval0 env exp'        -- partiality
+                            in IntVal (i + i')
+eval0 env (Abs name exp) = FunVal env name exp
+eval0 env (App exp exp') = let FunVal env' n body = eval0 env exp -- partiality
+                               val2 = eval0 env exp'
+                           in eval0 (M.insert n val2 env') body
