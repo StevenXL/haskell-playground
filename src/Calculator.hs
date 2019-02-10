ee56c6466fbc795e3ad70cacb3f9eb24fc706137
Steven Leiva <leiva.steven@gmail.com>
2019-02-09 18:27:03 -0600

    Add (partial) division to Calculator.
    The `evaluate` function is now a partial function, since it throws an
    error when evaluating the term `Div (Lit 1) (Lit 0)`.

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -8,15 +8,15 @@ data Exp
         Exp
   | Mult Exp
          Exp
+  | Div Exp
+        Exp
 
 evaluate :: Exp -> Int
 evaluate (Lit int) = int
 evaluate (Add expA expB) = evaluate expA + evaluate expB
 evaluate (Sub expA expB) = evaluate expA - evaluate expB
 evaluate (Mult expA expB) = evaluate expA * evaluate expB
-
-testExp1 :: Exp
-testExp1 = Add (Lit 1) (Sub (Lit 1) (Lit 2))
+evaluate (Div expA expB) = evaluate expA `div` evaluate expB
 
 showBinary :: String -> Exp -> Exp -> String
 showBinary str expA expB =
@@ -27,8 +27,22 @@ instance Show Exp where
   show (Add expA expB) = showBinary "Add" expA expB
   show (Sub expA expB) = showBinary "Sub" expA expB
   show (Mult expA expB) = showBinary "Mult" expA expB
+  show (Div expA expB) = showBinary "Div" expA expB
+
+testExp1 :: Exp
+testExp1 = Add (Lit 1) (Sub (Lit 1) (Lit 2))
+
+testExp2 :: Exp
+testExp2 = Div (Lit 1) (Lit 2)
+
+testExp3 :: Exp
+testExp3 = Div (Lit 1) (Lit 0)
 
 main :: IO ()
 main = do
   putStrLn (unwords ["Evaluating:", show testExp1])
   print (evaluate testExp1)
+  putStrLn (unwords ["Evaluating:", show testExp2])
+  print (evaluate testExp2)
+  putStrLn (unwords ["Evaluating:", show testExp3])
+  print (evaluate testExp3)
