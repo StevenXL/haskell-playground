aa6219e47148519197695af6c7c3a53c6266c4e0
Steven Leiva <leiva.steven@gmail.com>
2019-02-12 21:35:59 -0600

    Start our design with types.
    In Haskell, design often starts with types. Notice that our data
    declarations for `Token` and `Expression` have no data constructors, but
    this is still valid!

--- src/BasicsOfHaskell/SymbolicCalculator.hs
+++ src/BasicsOfHaskell/SymbolicCalculator.hs
@@ -1,1 +1,14 @@
 module BasicsOfHaskell.SymbolicCalculator where
+
+data Token
+
+data Expression
+
+tokenize :: String -> [Token]
+tokenize = undefined
+
+parse :: [Token] -> Expression
+parse = undefined
+
+evaluate :: Expression -> Double
+evaluate = undefined
