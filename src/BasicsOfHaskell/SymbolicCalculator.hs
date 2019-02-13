2d592bc471719271d6cbbd9d5357ff52c16fb858
Steven Leiva <leiva.steven@gmail.com>
2019-02-13 07:18:06 -0600

    Add operator function and implement tokenize.
    We have limited ourselves to parsing digits and single-character
    strings. This makes our job easier, and, while incomplete, moves us one
    step closer to the solution.
    
    Notice that both `tokenize` and `operator` are partial functions!

--- src/BasicsOfHaskell/SymbolicCalculator.hs
+++ src/BasicsOfHaskell/SymbolicCalculator.hs
@@ -1,5 +1,7 @@
 module BasicsOfHaskell.SymbolicCalculator where
 
+import qualified Data.Char as Char
+
 data Operator
   = Plus
   | Minus
@@ -16,10 +18,24 @@ data Token
 data Expression
 
 tokenize :: String -> [Token]
-tokenize = undefined
+tokenize [] = []
+tokenize (c:str)
+  | c `elem` "+-/*" = TokenOp (operator c) : tokenize str
+  | Char.isDigit c = TokenNumber (Char.digitToInt c) : tokenize str
+  | Char.isAlpha c = TokenIdentifier [c] : tokenize str
+  | Char.isSpace c = tokenize str
+  | otherwise = error (unwords ["Cannot tokenize charachter", [c], "."])
 
 parse :: [Token] -> Expression
 parse = undefined
 
 evaluate :: Expression -> Double
 evaluate = undefined
+
+operator :: Char -> Operator
+operator '+' = Plus
+operator '-' = Minus
+operator '*' = Mult
+operator '/' = Div
+operator c =
+  error (unwords ["Cannot convert character", [c], "into an operator"])
