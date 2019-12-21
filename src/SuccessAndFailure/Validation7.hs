8cd586bae7e85bc01a36e74d7490e2ccdb59dd70
Steven Leiva <leiva.steven@gmail.com>
2019-12-21 18:49:17 -0500

    Refactor our code to use `Validation`.
    Remember that `Validation` is isomorphic to `Either`. The reason we have
    `Validation` when we already have `Either` is that the `Applicative`
    instance for `Validation e` **accumulates** `Failure` values.
    
    In order to make use of `Validation e`'s accumulating behavior, we have
    to use the *sparrow* operators from the `Applicative` typeclass. I have
    never used these operators before. What I learned is that the sparrow
    operators ignore one of the values to which they are applied to, but,
    *crucially*, the sparrow operators do not ignore the effects of their
    operands. Like `Either`, we often use `Validation` to keep track of
    failure. The sparrow operators, for `Validation e`, will ignore one of
    the values **if** the value does not produce any errors, but it will
    concatenate the results of the "ignored" value with the "non-ignore"
    value if there are errors. This, for me at least, is the key takeaway of
    this book.

--- src/SuccessAndFailure/Validation7.hs
+++ src/SuccessAndFailure/Validation7.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module SuccessAndFailure.Validation7 where
 
@@ -8,6 +8,8 @@ import qualified Data.Char       as Char
 import qualified Data.List       as List
 import qualified Data.Validation as Validation
 
+import Data.Validation (Validation(..))
+
 main :: IO ()
 main = do
     userName <- prompt "Please enter your username." >> (Username <$> getLine)
@@ -15,70 +17,52 @@ main = do
     print (validateUsername userName)
     print (validatePassword pass)
 
-test :: IO ()
-test = printTest $ do
-           eq 1 (checkPasswordLength "") (Right $ Password "")
-           eq 2 (checkPasswordLength "julielovesbooks") (Right $ Password "julielovesbooks")
-
 -- VALIDATION FUNCTIONS
-makeUser :: Username -> Password -> Either Error User
+makeUser :: Username -> Password -> Validation Error User
 makeUser userName passWord = User <$> validateUsername userName <*> validatePassword passWord
 
-validatePassword :: Password -> Either Error Password
-validatePassword (Password pass) = do
-    cleaned    <- cleanWhitespace pass
-    withAlphas <- requireAlpha cleaned
-    checkPasswordLength withAlphas
+validatePassword :: Password -> Validation Error Password
+validatePassword (Password pass) = case cleanWhitespace pass of
+                                       Failure f -> Failure f
+                                       Success s -> requireAlpha s *> checkPasswordLength s
 
-validateUsername :: Username -> Either Error Username
-validateUsername (Username userName) = cleanWhitespace userName >>= requireAlpha >>= checkUsernameLength
+validateUsername :: Username -> Validation Error Username
+validateUsername (Username userName) = case cleanWhitespace userName of
+                                           Failure f -> Failure f
+                                           Success s -> requireAlpha s *> checkUsernameLength s
 
 makeError :: String -> Error
 makeError s = Error [s]
 
 -- TYPES
 data User = User Username Password
-newtype Error = Error [String] deriving (Show, Eq)
+newtype Error = Error [String] deriving (Show, Eq, Semigroup)
 newtype Password = Password String deriving (Show, Eq)
 newtype Username = Username String deriving Show
-
-instance Semigroup Error where
-    (<>) :: Error -> Error -> Error
-    (<>) (Error xs) (Error ys) = Error (xs <> ys)
 
 
 -- VALIDATION RULES
 
-checkPasswordLength :: String -> Either Error Password
+checkPasswordLength :: String -> Validation Error Password
 checkPasswordLength s = case (length s > 20 || length s < 10) of
-                               True  -> Left (makeError "Your password must be between 10 and 20 characters.")
-                               False -> Right (Password s)
+                               True  -> Failure (makeError "Your password must be between 10 and 20 characters.")
+                               False -> Success (Password s)
 
-requireAlpha :: String -> Either Error String
+requireAlpha :: String -> Validation Error String
 requireAlpha s = case (List.all Char.isAlphaNum s) of
-                     False -> Left (makeError "Your password cannot contain whitespace or special characters.")
-                     True  -> Right s
+                     False -> Failure (makeError "Your password cannot contain whitespace or special characters.")
+                     True  -> Success s
 
-cleanWhitespace :: String -> Either Error String
-cleanWhitespace "" = Left (makeError "Your password cannot be empty.")
+cleanWhitespace :: String -> Validation Error String
+cleanWhitespace "" = Failure (makeError "Your password cannot be empty.")
 cleanWhitespace s@(x:xs) = case (Char.isSpace x) of
                              True  -> cleanWhitespace xs
-                             False -> Right s
+                             False -> Success s
 
-checkUsernameLength :: String -> Either Error Username
+checkUsernameLength :: String -> Validation Error Username
 checkUsernameLength name = case (length name > 15) of
-                               True -> Left (makeError "Username cannot be longer than 15 characters.")
-                               False -> Right (Username name)
-
-checkLength :: Int -> String -> Either Error String
-checkLength maxLength s = case (length s > maxLength) of
-                              False -> Right s
-                              True -> Left $ makeError msg
-    where msg = List.intercalate " " [ "String"
-                                     , s
-                                     , "cannot be longer than"
-                                     , show maxLength
-                                     , "characters."]
+                               True -> Failure (makeError "Username cannot be longer than 15 characters.")
+                               False -> Success (Username name)
 
 prompt :: String -> IO ()
 prompt s = putStr $ s <> "\n> "
@@ -92,15 +76,15 @@ reverseLine = do
   print (reverse line)
 
 -- TESTING FRAMEWORK
-printTest :: Either String () -> IO ()
+printTest :: Validation String () -> IO ()
 printTest e = case e of
-                  Left s  -> putStrLn s
-                  Right _ -> putStrLn "All tests passed."
+                  Failure s  -> putStrLn s
+                  Success _ -> putStrLn "All tests passed."
 
-eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq :: (Eq a, Show a) => Int -> a -> a -> Validation String ()
 eq n actual expected = if actual == expected
-                           then Right ()
-                           else Left errMsg
+                           then Success ()
+                           else Failure errMsg
     where errMsg = unlines [ "Test " ++ show n
                            , "  Expected:    " ++ show expected
                            , "  But got:     " ++ show actual
